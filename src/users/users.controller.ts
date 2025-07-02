1dd986fc7fd9eeae2bab55c8e25c4ec358fbe7eb
Jaideep Ghungharwal <jdghungharwal04@gmail.com>
2025-07-02 17:21:29 +0530

    configuring http request for users, added global validation pipes till now

---
+++ src/users/users.controller.ts
@@ -0,0 +1,38 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Param,
+  Query,
+  Headers,
+  Ip,
+  ParseIntPipe,
+  DefaultValuePipe,
+} from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+
+@Controller('users')
+export class UsersController {
+  @Get('/{:id}')
+  public getUsers(
+    @Param('id', ParseIntPipe) id: number | undefined,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
+    console.log(typeof limit);
+    console.log(typeof page);
+    console.log(id, limit, page);
+    return 'You sent a GET request to users endpoint';
+  }
+
+  @Post()
+  public createUsers(
+    @Body() createUserDto: CreateUserDto,
+    @Headers() headers: any,
+    @Ip() ip: any,
+  ) {
+    console.log(createUserDto);
+    return 'You sent a POST request to users endpoint';
+  }
+}
